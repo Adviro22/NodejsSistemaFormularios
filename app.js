bbad46fb3e0daf8adcb2b36de6b3f8a8efc3fe0e
adviro22 <ajosueramos@gmail.com>
2023-04-25 21:28:09 -0500

    Mejoras al Proyecto

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
-require("dotenv").config();
 
-const {insert} = require('./operations');
-
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-const connection = mysql.createConnection({
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-})
 
-connection.connect((err) => {
-  if(err) throw err;
-  console.log("Connected to database");
+const dotenv = require('dotenv');
+dotenv.config({path:'./env/.env'});
 
-})
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+const bcryptjs = require('bcryptjs');
+
+const session = require('express-session');
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized:true
+}))
+
+const connection = require('./database/db');
 
 app.get("/", (req, res) => {
-  res.send("Hola Mundo")
+  res.render('index')
 })
 
-app.get("/insert", (req, res) => {
-  insert(connection, (result) => {
-    res.json(result);
-  });
-});
+//Autentication
+app.post('/auth', async(req, res) =>{
+  const user = req.body.user;
+  const pass = req.body.pass;
+  let passwordHaash = await bcryptjs.hash(pass, 8);
+  if( user && pass){
+    connection.query('SELECT * FROM users WHERE user = ?', [user], async(error, results) =>{
+      if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+        res.send('Usuario o Contraseña Incorrecta');
+      }else{
+        res.send('lOGIN CORRECTO');
+      }
+    })
+  }
+})
 
 app.listen(3000, () => {
   console.log("Servidor en el puerto 3000...");
